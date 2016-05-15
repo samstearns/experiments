7ab29b54f8b9f03ed1eb9daff907e3cf50be847f
Sam Stearns <sam.stearns@gmail.com>
2016-05-15 13:59:22 -0400

    Initial checkin

---
+++ dow-cluster/dow-cluster.R
@@ -0,0 +1,28 @@
+library(quantmod)
+
+# http://money.cnn.com/data/dow30/
+dow.tickers <- c("MMM", "AXP", "AAPL", "BA", "CAT", 
+                 "CVX", "CSCO", "KO", "DIS", "DD", 
+                 "XOM", "GE", "GS", "HD", "IBM", 
+                 "INTC", "JNJ", "JPM", "MCD", "MRK",
+                 "MSFT", "NKE","PFE","PG", "TRV", 
+                 "UTX", "UNH", "VZ", "V", "WMT")
+
+# load daily pricing data and store it in a dataframe
+getSymbols(dow.tickers)
+
+# load daily returns for 2015 in a dataframe
+returns <- data.frame(matrix(ncol = 30, nrow = 252))
+names(returns) <- dow.tickers
+
+for (i in 1:length(dow.tickers)) {
+  ticker <- get(dow.tickers[i])
+  returns[,i] <- periodReturn(ticker, period="daily", subset='2015')
+}
+
+# The distance matrix determines similarity between rows. 
+# Transpose the values which are store in a column per ticker
+dist.matrix.input <- t(as.matrix(returns))
+clust.complete <- hclust(dist(dist.matrix.input), method="complete")
+
+plot(clust.complete, main="Complete Linkage")
