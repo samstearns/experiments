ac118e6d388f49d544d52abfd8dee47ccf5992e5
Sam Stearns <sam.stearns@gmail.com>
2016-09-06 21:42:53 -0400

    Refactoring

--- sp500returns/sp500.R
+++ sp500returns/sp500.R
@@ -9,48 +9,34 @@ setwd("/Users/sjs/Dropbox/dev/git/experiments/sp500returns/")
 sp <-read.csv("./sp500.csv")
 
 # Helper function to calculate CAGRs
-cagr <- function(end, start, years)
-{
+CalculateCAGR <- function(end, start, years) {
   return ((end / start) ** (1 / years) - 1.0)
 }
 
-# calculate annual returns
-annual <- c()
-for (i in 2:89) {
-  annual[i-1] = cagr(sp[i,2], sp[i-1,2], 1)
+# Helper function to calculate CAGRs
+CalculateRollingReturns <- function(start, end, lookback) {
+  
+  annual.returns <-c()
+  for (i in start:end) {
+    annual.returns[i] = CalculateCAGR(sp[i,2], sp[i-lookback,2], lookback)
+  }
+  
+  return(annual.returns)
 }
 
-# calculate three year returns
-three.year <- c()
-for (i in 4:89) {
-    three.year[i-1] = cagr(sp[i,2], sp[i-3,2], 3)
-}
+# Calculate Rolling returns
+result.frame = as.data.frame(matrix(ncol=6, nrow=89))
+names(result.frame) = c("One Year", "3 Year", "5 Year", "10 Year", "20 Year", "30 Year")
+result.frame[["One Year"]] <- CalculateRollingReturns(31,89, 1)
+result.frame[["3 Year"]] <- CalculateRollingReturns(31,89, 3)
+result.frame[["5 Year"]] <- CalculateRollingReturns(31,89, 5)
+result.frame[["10 Year"]] <- CalculateRollingReturns(31,89, 10)
+result.frame[["20 Year"]] <- CalculateRollingReturns(31,89, 20)
+result.frame[["30 Year"]] <- CalculateRollingReturns(31,89, 30)
 
-# calculate five year returns
-five.year <- c()
-for (i in 6:89) {
-  five.year[i-1] = cagr(sp[i,2], sp[i-5,2], 5)
-}
+# GG Plot
+ggplot2(result.frame) + geom_boxplot()
 
-# calculate 10 year returns
-ten.year <- c()
-for (i in 11:89) {
-  ten.year[i-1] = cagr(sp[i,2], sp[i-10,2], 10)
-}
-
-# calculate 20 year returns
-twenty.year <- c()
-for (i in 21:89) {
-  twenty.year[i-1] = cagr(sp[i,2], sp[i-20,2], 20)
-}
-
-# calculate 30 year returns
-thirty.year <- c()
-for (i in 31:89) {
-  thirty.year[i-1] = cagr(sp[i,2], sp[i-30,2], 30)
-}
-
-boxplot(annual, three.year, five.year, ten.year, twenty.year, thirty.year, horizontal = TRUE)
-
-title(main="S&P 500 Returns", sub="Distribution of rolling returns, excluding dividends", xlab = "Annual Return", 
-      ylab = c("1 Year", "3 Year", "5 Year", "10 Year", "20 Year", "30 Year"))
+# Standard plot
+boxplot(result.frame, horizontal = TRUE)
+title(main="S&P 500 Returns", sub="Distribution of rolling returns, excluding dividends", xlab = "Annual Return")
