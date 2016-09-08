97a6bd6dd73565bf5a4498ac4c226996bcd68301
Sam Stearns <sam.stearns@gmail.com>
2016-09-07 20:36:06 -0400

    Updates
    GGPlot fixes
    Simplified indexes

--- sp500returns/sp500.R
+++ sp500returns/sp500.R
@@ -1,9 +1,6 @@
 # Create a boxplot of rolling returns for the S&P: 1, 5, 10, 20, 30 years
-
-# TODO: Use the shift function in the data.table library to simplify code
-# http://stackoverflow.com/questions/14689424/use-a-value-from-the-previous-row-in-an-r-data-table-calculation
-library(data.table)
 library(ggplot2)
+library(scales)
 
 setwd("/Users/sjs/Dropbox/dev/git/experiments/sp500returns/")
 sp <-read.csv("./sp500.csv")
@@ -19,14 +16,14 @@ CalculateRollingReturns <- function(start, end, lookback) {
   
   annual.returns <-c()
   for (i in start:end) {
-    annual.returns[i] = CalculateCAGR(sp[i,2], sp[i-lookback,2], lookback)
+    annual.returns[i-start] = CalculateCAGR(sp[i,2], sp[i-lookback,2], lookback)
   }
   
   return(annual.returns)
 }
 
 # Calculate Rolling returns
-result.frame = as.data.frame(matrix(ncol=6, nrow=89))
+result.frame = as.data.frame(matrix(ncol=6, nrow=58))
 names(result.frame) = c("One Year", "3 Year", "5 Year", "10 Year", "20 Year", "30 Year")
 result.frame[["One Year"]] <- CalculateRollingReturns(31,89, 1)
 result.frame[["3 Year"]] <- CalculateRollingReturns(31,89, 3)
@@ -35,10 +32,14 @@ result.frame[["10 Year"]] <- CalculateRollingReturns(31,89, 10)
 result.frame[["20 Year"]] <- CalculateRollingReturns(31,89, 20)
 result.frame[["30 Year"]] <- CalculateRollingReturns(31,89, 30)
 
-# GG Plot
-p <- ggplot(result.frame) 
-p + geom_boxplot() + coord_flip()
-
 # Standard plot
 boxplot(result.frame, horizontal = TRUE)
-title(main="S&P 500 Returns", sub="Distribution of rolling returns, excluding dividends", xlab = "Annual Return")
+title(main = "S&P 500 Returns", sub = "Distribution of rolling returns, excluding dividends", xlab = "Annual Return")
+
+# GG Plot. Use levels as factor to order the columns
+p <- ggplot(stack(result.frame), aes(x = factor(ind, levels = names(result.frame)), y = values))
+p + geom_boxplot() + coord_flip() + labs(title = "S&P 500 Rolling Returns", x="", y="Annnual Return (Excluding dividends)")
+# TODO: Fix label formatting
+
+# TODO: Use the shift function in the data.table library to simplify code
+# http://stackoverflow.com/questions/14689424/use-a-value-from-the-previous-row-in-an-r-data-table-calculation
