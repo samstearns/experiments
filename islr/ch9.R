0512a3ce0951862c0049c23b4a072cf516a5cbd8
Sam Stearns <sam.stearns@gmail.com>
2016-12-06 19:43:15 -0500

    Interim check-in

--- islr/ch9.R
+++ islr/ch9.R
@@ -27,3 +27,106 @@ summary(svmfit)
 svmfit = svm(y ~ ., data = dat, kernel = "linear", cost = 0.1, scale = FALSE)
 plot(svmfit, dat)
 summary(svmfit)
+
+# Use tune function to perform cross-validation
+set.seed(1)
+tune.out = tune(svm, y ~ ., data = dat, kernel = "linear",
+                ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
+summary(tune.out)
+
+# access best model
+bestmod = tune.out$best.model
+summary(bestmod)
+
+# Use predict function to predict class label on a set of test observations
+# Use best model identified in cross-validation
+xtest = matrix(rnorm(20*2), ncol=2)
+ytest = sample(c(-1,1), 20, rep=TRUE)
+xtest[ytest==1,] = xtest[ytest==1,] + 1
+testdat = data.frame(x = xtest, y = as.factor(ytest))
+
+ypred = predict(bestmod, testdat)
+table(predict = ypred, truth = testdat$y)
+
+# Repredict with different cost function
+svmfit = svm(y ~ ., data = dat, kernel = "linear", cost = .01, scale = FALSE)
+ypred = predict(svmfit, testdat)
+table(predict = ypred, truth = testdat$y)
+
+# Find a separating hyperplane using the SVM function. First
+# Futher separate the two classes so they are linearly separable
+x[y==1,] = x[y==1,] + 0.5
+plot(x, col = (y+5)/2, pch = 19)
+
+# Fit the classifer using large cost value to prevent mis-classifications
+dat = data.frame(x=x, y = as.factor(y))
+svmfit = svm(y ~ ., data = dat, kernel="linear", cost = 1e5)
+summary(svmfit)
+plot(svmfit, dat)
+
+# Use a smaller value of cost
+svmfit = svm(y ~ ., data = dat, kernel="linear", cost = 1)
+summary(svmfit)
+plot(svmfit, dat)
+
+
+###############################################################################
+# 9.6.2 Support Vector Machine
+###############################################################################
+
+# First generate data with nonlinear class boundary
+set.seed(1)
+x = matrix(rnorm(200*2), ncol = 2)
+x[1:100,] = x[1:100,] + 2
+x[101:150,] = x[101:150,] - 2
+y = c(rep(1, 150), rep(2, 50))
+dat = data.frame(x=x, y = as.factor(y))
+plot(x, col=y)
+
+# Randomly split into training and test groups
+train = sample(200, 100)
+svmfit = svm(y ~ ., data = dat[train,], kernel = "radial", gamma = 1, cost = 1)
+plot(svmfit, dat[train,])
+summary(svmfit)
+
+# Increase cost to reduce training errors. But more irregular boundary may be at risk
+# of overfitting
+svmfit = svm(y ~ ., data = dat[train,], kernel = "radial", gamma = 1, cost = 1e5)
+plot(svmfit, dat[train,])
+
+# Perform cross validation to select best choice of gamma and cost
+set.seed(1)
+tune.out = tune(svm, y ~ ., 
+                data = dat[train,], 
+                kernel = "radial",
+                ranges = list(cost = c(0.1, 1, 10, 100, 1000),
+                              gamma = c(0.5, 1, 2, 3, 4)))
+
+summary(tune.out)
+
+# Apply predictions using best parameters
+table(true = dat[-train, "y"], pred = predict(tune.out$bestmodel, newdata = dat[-train,]))
+
+###############################################################################
+# 9.6.3 ROC Curves
+###############################################################################
+
+library(ROCR)
+
+# Function to plot ROC curve given vector with numerical score pred and class labels truth
+rocplot <- function(pred, truth, ...) {
+  predob = prediction(pred, truth)
+  perf = performance(predob, "tpr", "fpr")
+  plot(perf, ...)
+}
+
+svmfit.opt = svm(y ~ ., data = dat[train,], kernel = "radial", gamma = 2, cost = 1, decision.values = T)
+predict(svmfit.opt, dat[train,])
+fitted = attributes(predict(svmfit.opt, dat[train,], decision.values = TRUE))$decision.values
+
+
+###############################################################################
+# 9.6.4 SVM with Multiple Classes
+###############################################################################
+set.seed(1)
+x = rbind(x)
