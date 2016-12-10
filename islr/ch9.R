68c145c5d618f8723a56762c026d877d819e0f29
Sam Stearns <sam.stearns@gmail.com>
2016-12-10 15:12:25 -0500

    Completed 9.6.4 and 9.6.5

--- islr/ch9.R
+++ islr/ch9.R
@@ -157,5 +157,47 @@ rocplot(fitted, dat[-train, "y"], add = T, col="red")
 ###############################################################################
 # 9.6.4 SVM with Multiple Classes
 ###############################################################################
+
+# If the response has > 2 levels, svm() will perform multi-class classification
+# Generate a third class of observations
 set.seed(1)
-x = rbind(x)
+x = rbind(x, matrix(rnorm(50*2), ncol = 2))
+y = c(y, rep(0, 50))
+x[y==0, 2] = x[y==0, 2] + 2
+dat = data.frame(x=x, y = as.factor(y))
+par(mfrow = c(1,1 ))
+plot(x, col = (y+1))
+
+# Fit an SVM to the data
+svmfit = svm(y ~ ., data = dat, kernel = "radial", cost = 10, gamma = 1)
+plot(svmfit, dat)
+
+###############################################################################
+# 9.6.5 Application to Gene Expression Data
+###############################################################################
+
+# Examine the Khan data set, which consists of tissue samples for four types of 
+# small round blue cell tumors. Gene expression measurements are available
+# Examine the datasets. Measurements for 2,308 genes. Training = 63 observations.
+# Test = 20 observations
+library(ISLR)
+names(Khan)
+dim(Khan$xtrain)
+dim(Khan$xtest)
+length(Khan$ytrain)
+length(Khan$ytest)
+
+# Create support vector approach to predict Cancer subtype
+# There is a large # of features relative to observations. Use a linear kernel
+# because additional flexibility from polynomial, radial kernel is unnecessary
+dat = data.frame(x = Khan$xtrain, y = as.factor(Khan$ytrain))
+out = svm(y ~ ., data = dat, kernel = "linear", cost = 10)
+
+# There are no training errors. This is not suprising, given the large # of variables
+# vs observations makes it easier to find hyperplanes that separate the classes
+summary(out)
+
+# Run on test observations. Cost = 10 leads to two test set errors
+dat.te = data.frame(x = Khan$xtest, y = as.factor(Khan$ytest))
+pred.te = predict(out, newdata = dat.te)
+table(pred.te, dat.te$y)
