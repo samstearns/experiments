ef1caa8255ee418ddc70b93216f42bcfa3dc1775
Sam Stearns <sam.stearns@gmail.com>
2017-03-09 22:12:51 -0500

    Modeling future admission counts

--- duncan/duncan-examples.R
+++ duncan/duncan-examples.R
@@ -31,6 +31,11 @@ hcc.prospective.data <- duncan.data[, c(2, 21, 32, 51:133)]
 hcc.prospective.fit <- lm(allow_future_total ~ ., data = hcc.prospective.data)
 summary(hcc.prospective.fit)
 
+hcc.prospective.admit.data <- duncan.data[, c(2, 21, 35, 51:133)]
+hcc.prospective.admit.fit <- lm(admit_cnt_future ~ ., data = hcc.prospective.admit.data)
+summary(hcc.prospective.admit.fit)
+
+
 hcc.prior.cost.data <- duncan.data[, c(2, 21, 26, 32, 51:133)]
 hcc.prior.cost.fit <- lm(allow_future_total ~ ., data = hcc.prior.cost.data)
 summary(hcc.prior.cost.fit)
