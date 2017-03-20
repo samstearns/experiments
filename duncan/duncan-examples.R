bfc0b3ffc710c0a2161523b41291f2b29c8ff863
Sam Stearns <sam.stearns@gmail.com>
2017-03-19 20:21:30 -0400

    Moving test models to separate file

--- duncan/duncan-examples.R
+++ duncan/duncan-examples.R
@@ -13,32 +13,6 @@ summary(duncan.data)
 # 8.7: Example of linear regressions ----------------------------------------------------------
 hist(allow_future_total)
 
-age.gender.prospective.fit <- lm(allow_future_total ~ gender + age, data = duncan.data)
-summary(age.gender.prospective.fit)
-
-prior.cost.prospective.fit <- lm(allow_future_total ~ allow_current_total, data = duncan.data)
-summary(prior.cost.prospective.fit)
-
-prior.cost.prospective.2.fit <- lm(allow_future_total ~ allow_current_total + non_claimant, data = duncan.data)
-summary(prior.cost.prospective.2.fit)
-
-# Simple Age/Gender + HCCs
-hcc.current.data <- duncan.data[, c(2, 21, 26, 51:133)]
-hcc.concurrent.fit <- lm(allow_current_total ~ ., data = hcc.current.data)
-summary(hcc.concurrent.fit)
-
-hcc.prospective.data <- duncan.data[, c(2, 21, 32, 51:133)]
-hcc.prospective.fit <- lm(allow_future_total ~ ., data = hcc.prospective.data)
-summary(hcc.prospective.fit)
-
-hcc.prospective.admit.data <- duncan.data[, c(2, 21, 35, 51:133)]
-hcc.prospective.admit.fit <- lm(admit_cnt_future ~ ., data = hcc.prospective.admit.data)
-summary(hcc.prospective.admit.fit)
-
-hcc.prior.cost.data <- duncan.data[, c(2, 21, 26, 32, 51:133)]
-hcc.prior.cost.fit <- lm(allow_future_total ~ ., data = hcc.prior.cost.data)
-summary(hcc.prior.cost.fit)
-
 # 10.3: Example of Logistic Regression to predict likelihood of hospitalization ---------------
 admission.table <- table(duncan.data$admit_flg_future, duncan.data$admit_flg_current)
 admission.table
@@ -47,3 +21,25 @@ attach(duncan.data)
 
 loh.fit = glm(admit_flg_future ~ admit_flg_current + gender + A_OVER64 + Er_visit_flg_current + pcp_visit_cnt_current, data=duncan.data, family=binomial)
 summary(loh.fit)
+
+
+# Regression trees to predict prospective cost
+library(MASS)
+library(tree)
+
+set.seed(1)
+train = sample(1:nrow(hcc.prospective.data), nrow(hcc.prospective.data)/2)
+tree.prospective = tree(allow_future_total ~., hcc.prospective.data, subset=train)
+summary(tree.prospective)
+
+# Use unpruned tree to make predictions on the test set
+yhat = predict(tree.prospective, newdata = hcc.prospective.data[-train ,])
+hcc.prospective.costs = hcc.prospective.data[-train ,"allowed_future_cost"]
+plot(yhat, boston.test)
+abline(0,1)
+
+# Test set MSE associated with the regression tree is 25.05. 
+# The square root of the MSE is therefore around 5.005, 
+# indicating that this model leads to test predictions that 
+# are within around $5,005 of the true median home value for the suburb.
+mean((yhat-hcc.prospective.costs)^2)
---
+++ duncan/sample-models.R
@@ -0,0 +1,55 @@
+###############################################################################################
+# Risk Adjustment Benchmark models
+# This script uses the test dataset accompanying Healthcare Risk Adjustment and Predictive Modeling, by Ian Duncan
+# The test dataset contains 10,000 randomly selected patients from a commercial population
+# The 10,000 patients are continuously enrolled for two years
+###############################################################################################
+
+
+# Data Setup ----------------------------------------------------------------------------------
+setwd("/Users/sjs/dev/git/experiments/duncan")
+duncan.data <- read.csv("modeling_sample_dataset.csv")
+summary(duncan.data)
+
+# Helper functions ----------------------------------------------------------------------------
+# mean((yhat-hcc.prospective.costs)^2)
+
+CalculateMSE <- function (model, test.data, actual.values) {
+  
+  yhat = predict(model, test.data)
+  
+  mse <- mean((yhat - actual.values)^2)
+}
+
+# Concurrent models ----------------------------------------------------------------------------
+hcc.current.data <- duncan.data[, c(2, 21, 26, 51:133)]
+hcc.concurrent.fit <- lm(allow_current_total ~ ., data = hcc.current.data)
+summary(hcc.concurrent.fit)
+foo <- CalculateMSE(hcc.concurrent.fit, hcc.current.data, hcc.current.data$allow_current_total)
+
+# Prospective models ----------------------------------------------------------------------------
+
+age.gender.prospective.fit <- lm(allow_future_total ~ gender + age, data = duncan.data)
+summary(age.gender.prospective.fit)
+
+prior.cost.prospective.fit <- lm(allow_future_total ~ allow_current_total, data = duncan.data)
+summary(prior.cost.prospective.fit)
+
+prior.cost.prospective.2.fit <- lm(allow_future_total ~ allow_current_total + non_claimant, 
+                                   data = duncan.data)
+summary(prior.cost.prospective.2.fit)
+
+
+# Simple Age/Gender + HCCs
+
+hcc.prospective.data <- duncan.data[, c(2, 21, 32, 51:133)]
+hcc.prospective.fit <- lm(allow_future_total ~ ., data = hcc.prospective.data)
+summary(hcc.prospective.fit)
+
+hcc.prospective.admit.data <- duncan.data[, c(2, 21, 35, 51:133)]
+hcc.prospective.admit.fit <- lm(admit_cnt_future ~ ., data = hcc.prospective.admit.data)
+summary(hcc.prospective.admit.fit)
+
+hcc.prior.cost.data <- duncan.data[, c(2, 21, 26, 32, 51:133)]
+hcc.prior.cost.fit <- lm(allow_future_total ~ ., data = hcc.prior.cost.data)
+summary(hcc.prior.cost.fit)
