8f96e4eda53c3dd3e7695daf2e56dc81b61de15f
Sam Stearns <sam.stearns@gmail.com>
2018-04-26 22:41:29 -0400

    Updated code style

--- duncan/sample-models.R
+++ duncan/sample-models.R
@@ -8,45 +8,45 @@
 library(tree)
 
 # Data Setup ----------------------------------------------------------------------------------
-duncan.data <- read.csv("modeling_sample_dataset.csv")
-summary(duncan.data)
+duncan_data <- read.csv("modeling_sample_dataset.csv")
+summary(duncan_data)
 
 # Helper functions ----------------------------------------------------------------------------
 # mean((yhat-hcc.prospective.costs)^2)
-CalculateMSE <- function (model, test.data, actual.values) {
-  yhat = predict(model, test.data)
-  mse <- mean((yhat - actual.values)^2)
+calculate_mse <- function (model, test_data, actual_values) {
+  yhat = predict(model, test_data)
+  mse <- mean((yhat - actual_values)^2)
 }
 
 # Concurrent models ----------------------------------------------------------------------------
-hcc.current.data <- duncan.data[, c(2, 21, 26, 51:133)]
-hcc.concurrent.fit <- lm(allow_current_total ~ ., data = hcc.current.data)
-summary(hcc.concurrent.fit)
-foo <- CalculateMSE(hcc.concurrent.fit, hcc.current.data, hcc.current.data$allow_current_total)
+hcc_current_data <- duncan_data[, c(2, 21, 26, 51:133)]
+hcc_concurrent_fit <- lm(allow_current_total ~ ., data = hcc_current_data)
+summary(hcc_concurrent_fit)
+foo <- calculate_mse(hcc_concurrent_fit, hcc_current_data, hcc_current_data$allow_current_total)
 
 # Prospective models ----------------------------------------------------------------------------
-age.gender.prospective.fit <- lm(allow_future_total ~ gender + age, data = duncan.data)
-summary(age.gender.prospective.fit)
+age_gender_prospective_fit <- lm(allow_future_total ~ gender + age, data = duncan_data)
+summary(age_gender_prospective_fit)
 
-prior.cost.prospective.fit <- lm(allow_future_total ~ allow_current_total, data = duncan.data)
-summary(prior.cost.prospective.fit)
+prior_cost_prospective_fit <- lm(allow_future_total ~ allow_current_total, data = duncan_data)
+summary(prior_cost_prospective_fit)
 
-prior.cost.prospective.2.fit <- lm(allow_future_total ~ allow_current_total + non_claimant, data = duncan.data)
-summary(prior.cost.prospective.2.fit)
+prior_cost_prospective_2_fit <- lm(allow_future_total ~ allow_current_total + non_claimant, data = duncan_data)
+summary(prior_cost_prospective_2_fit)
 
 # Simple Age/Gender + HCCs
-hcc.prospective.data <- duncan.data[, c(2, 21, 32, 51:133)]
-hcc.prospective.fit <- lm(allow_future_total ~ ., data = hcc.prospective.data)
-summary(hcc.prospective.fit)
+hcc_prospective_data <- duncan_data[, c(2, 21, 32, 51:133)]
+hcc_prospective_fit <- lm(allow_future_total ~ ., data = hcc_prospective_data)
+summary(hcc_prospective_fit)
 
-hcc.prior.cost.data <- duncan.data[, c(2, 21, 26, 32, 51:133)]
-hcc.prior.cost.fit <- lm(allow_future_total ~ ., data = hcc.prior.cost.data)
-summary(hcc.prior.cost.fit)
+hcc_prior_cost_data <- duncan_data[, c(2, 21, 26, 32, 51:133)]
+hcc_prior_cost_fit <- lm(allow_future_total ~ ., data = hcc_prior_cost_data)
+summary(hcc_prior_cost_fit)
 
-tree.prospective =tree(allow_future_total ~ ., data = hcc.prospective.data)
-summary (tree.prospective)
+tree_prospective = tree(allow_future_total ~ ., data = hcc_prospective_data)
+summary (tree_prospective)
 
 # Prospective Admissions models ----------------------------------------------------------------------------
-hcc.prospective.admit.data <- duncan.data[, c(2, 21, 35, 51:133)]
-hcc.prospective.admit.fit <- lm(admit_cnt_future ~ ., data = hcc.prospective.admit.data)
-summary(hcc.prospective.admit.fit)
+hcc_prospective_admit_data <- duncan_data[, c(2, 21, 35, 51:133)]
+hcc_prospective_admit_fit <- lm(admit_cnt_future ~ ., data = hcc_prospective_admit_data)
+summary(hcc_prospective_admit_fit)
